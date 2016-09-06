fb22c997706cd44dd8c852fce148c84236527ffa
Muntadher AlZayer - PC <mual3154@colorado.edu>
2016-09-05 19:43:18 -0600

    finished assignment

--- HW1.cpp
+++ HW1.cpp
@@ -14,97 +14,109 @@ struct wordItem{
 };
 
 
-/*
-
-* Function name: getStopWords
-
-* Purpose: read stop word list from file and store into vector
-
-* @param ignoreWordFile - filename (the file storing ignore words)
-
-* @param _vecIgnoreWords - store ignore words from the file (pass by reference)
-
-* @return - none
-
-* Note: The number of words is fixed to 50
-
-*/
 
 void getStopWords(char *ignoreWordFileName, vector<string>& _vecIgnoreWords);
-
-/*
-
-* Function name: isCommonWord
-
-* Purpose: to see if a word is a common word
-
-* @param word - a word (which you want to check if it is a common word)
-
-* @param _vecIgnoreWords - the vector type of string storing ignore/common words
-
-* @return - true (if word is a common word), or false (otherwise)
-
-*/
-
 bool isCommonWord(string word, vector<string>& _vecIgnoreWords);
-
-/*
-
-* Function name: printTopN
-
-* Purpose: to print the top N high frequency words
-
-* @param wordItemList - a pointer that points to an array of wordItems
-
-* @param topN - the number of top frequency words to print
-
-* @return none
-
-*/
-
 void printTopN(wordItem wordItemList[], int topN);
-
 const int STOPWORD_LIST_SIZE = 50;
-
-// ./a.out 10 HW1-HungerGames_edit.txt HW1-ignoreWords.txt
 
 int main(int argc, char* argv[])
 {
-	
-	// verify we have the correct # of parameters, else throw error msg & return
-	
 	if (argc != 4)
-	
 	{
-	
 	cout << "Usage: ";
-	
 	cout << argv[0] << " <number of words> <filename.txt> <ignorefilename.txt>";
-	
 	cout << endl;
-	
 	return 0;
-
 	}
+	
 	//end of parameter check --------------------------------------------------------------------
+	int size = 100;
+	wordItem *library = new wordItem [size];
 	int range = stoi(argv[1]);
 	string mainFile = argv[2];
-	string ignoreFile = argv[3];
-	cout << range << " " << mainFile << " " << ignoreFile << endl;
+	//string ignoreFile = argv[3];
 	vector<string> vecIgnoreWords(STOPWORD_LIST_SIZE);
-	getStopWords(ignoreFile, vecIgnoreWords);
+	getStopWords(argv[3], vecIgnoreWords);
+	string ind;
+	int totwrds = 0;
+	int unqwrds = 0;
+	int v = 0;
+	int aindex = 0;
+	int dbscnt = 0;
+
+	ifstream file;
+	file.open(mainFile);
+	while(file >> ind)
+	{
+		totwrds++;
+		if(file.is_open())
+		{
+			bool a = isCommonWord(ind , vecIgnoreWords);
+			if(a == false)
+			{
+				unqwrds++;
+				for(int i = 0; i<=aindex; i++)
+				{
+					if(ind == library[i].word)
+					{
+						library[i].count++;
+						v=1;//resets while loop
+					}
+				}
+				if(v==0)//adds word if not in library
+					{	
+						library[aindex].word = ind;
+						library[aindex].count = 1;
+						aindex++;
+					}
+					for(int z=0;z<size;z++)//ensures that the library is in descending order
+					{
+						if (library[z+1].count > library[z].count)
+						{
+							int temp = 0;
+							string tem;
+							temp = library[z].count;
+							library[z].count = library[z+1].count;
+							library[z+1].count = temp;
+							tem = library[z].word;
+							library[z].word = library[z+1].word;
+							library[z+1].word = tem;
+						}
+					}
+				if (size <= aindex)//here we double
+					{
+						dbscnt++;
+    					wordItem *newArray = new wordItem[2*size];
+
+    					for (int i=0; i<size; i++)
+    					{
+        					newArray[i].count = library[i].count;
+        					newArray[i].word = library[i].word;
+    					}
+    							size*=2;
+    							delete[] library;
+    							library = newArray;	    
+					}
+				
+			}
+		}
+	v = 0;
+
+	}
 	
 	
+	printTopN(library, range);
+	cout<<"#"<<endl;
+	cout<<"Array doubled: "<<dbscnt<<endl;
+	cout<<"#"<<endl;
+	cout<<"Unique non-common words: "<<aindex<<endl;
+	cout<<"#"<<endl;
+	cout<<"Total non-common words: "<<unqwrds<<endl;
+	
+	file.close();
+	delete[] library;
 
-/* **********************************
-
-1. Implement your code here.
-
-Read words from HW1-HungerGames_edit.txt, store and count the words.
-
-2. Implement the three functions after the main() function seperately.
-
-********************************** */
 	return 0;
 
 }
@@ -112,36 +124,39 @@ Read words from HW1-HungerGames_edit.txt, store and count the words.
 void getStopWords(char *ignoreWordFileName, vector<string>& _vecIgnoreWords)
 
 {
-	string line;
-	ignoreWordFileName.open(ignoreWordFileName);
-	if(ignoreWordFileName.is_open())
+	int i = 0;
+	string igword;
+	ifstream ignore;
+	ignore.open(ignoreWordFileName);
+	while(ignore >> igword)
 	{
-		while(getline(cin, line))
-		{
-		_vecIgnoreWords.push_back(line);	
-		}
-	
+		if(ignore.is_open())
+	      {
+            _vecIgnoreWords[i] = igword;
+            i++;
+	      }
 	}
-	else
-	{
-		cout << "File unable to open!" << endl;
-	}
-	return;
-
+	ignore.close();
 }
 
 bool isCommonWord(string word, vector<string>& _vecIgnoreWords)
 
 {
-
-	return true;
-
+	for(int i = 0; i<STOPWORD_LIST_SIZE; i++)
+	{
+		if(word == _vecIgnoreWords[i])
+		{
+			return true;
+		}
+	}
+	return false;
 }
 
 void printTopN(wordItem wordItemList[], int topN)
 
 {
-	for(int x = 0 ; x<topN ; x++){
+	for(int x = 0 ; x<topN ; x++)
+	{
 		cout << wordItemList[x].word << " - " << wordItemList[x].count << endl;
 	}
 	return;
--- test.cpp
+++ test.cpp
@@ -1,36 +1,252 @@
-#include<iostream>
-#include<fstream>
-#include<sstream>
-#include<string>
-#include<cstdlib>
-#include<vector>
+nclude <iostream>
+#include <string>
+#include <fstream>
+#include <sstream> 
+#include <math.h>
+#include <string>
+#include <vector>
+
 
 using namespace std;
 
-struct wordItem{
+//int check(string ind);
+//int *dubs(int array[], int size, int number);
+struct wordItem//structure for the words
+{
 	string word;
 	int count;
-	
 };
 
-int main(int argc, char* argv[])
+void getStopwords(char *ignoreWordFileName, vector<string>& _vecIgnoreWords);
+void printTopN(wordItem *&wordItemList, int topN);
+bool isCommonWord(string word, vector<string>& _vecIgnoreWords);
+const int STOPWORD_LIST_SIZE = 50;
+
+
+int main(int argc, char *argv[])
 {
-	string ignoreWordFileName = argv[1];
-	string line;
-	ifstream infile;
-	infile.open(ignoreWordFileName);
-	vector<string> _vecIgnoreWords(50); 
-	if(infile.is_open())
-	{
-		while(getline(infile, line))
-		{
-		cout << line << endl;
-		_vecIgnoreWords.push_back(line);	
-		}
+	int size = 100;//initialize array/size
+	wordItem *library = new wordItem[size];//use later for doubling
 	
+	vector<string> vecIgnoreWords(STOPWORD_LIST_SIZE);
+	int t = atoi(argv[1]);//This changes the command line argument to an integer
+	getStopwords(argv[3], vecIgnoreWords);
+	string ind;
+	int totwrds = 0;//total words in document
+	int unqwrds = 0;//total "unique" words
+	int v=0;//
+	int aindex=0;
+	int dbscnt=0;
+
+	
+	ifstream file;
+	file.open(argv[2]);
+	while(file >> ind)
+	{	
+		totwrds = totwrds + 1;
+		if(file.is_open())
+		{
+			bool a = isCommonWord(ind, vecIgnoreWords);
+			if (a == false)//only continues if the word is unique
+			{
+				unqwrds++;
+				for(int i = 0; i<=aindex; i++)//if the word is already in library the counter goes up
+				{
+					if(ind == library[i].word)
+					{
+						library[i].count++;
+						v=1;//resets while loop
+					}
+				}
+				if(v==0)//adds word if not in library
+					{	
+						library[aindex].word = ind;
+						library[aindex].count = 1;
+						aindex++;
+					}
+					for(int z=0;z<size;z++)//ensures that the library is in descending order
+					{
+						if (library[z+1].count > library[z].count)
+						{
+							int temp = 0;
+							string tem;
+							temp = library[z].count;
+							library[z].count = library[z+1].count;
+							library[z+1].count = temp;
+							tem = library[z].word;
+							library[z].word = library[z+1].word;
+							library[z+1].word = tem;
+						}
+					}
+				if (size <= aindex)//here we double
+					{
+						dbscnt++;
+    					wordItem *newArray = new wordItem[2*size];
+
+    					for (int i=0; i<size; i++)
+    					{
+        					newArray[i].count = library[i].count;
+        					newArray[i].word = library[i].word;
+    					}
+    							size*=2;
+    							delete[] library;
+    							library = newArray;	    
+					}
+				
+			}
+		}
+	v=0;
+
 	}
-	else
+	printTopN(library, t);
+	cout<<"#"<<endl;
+	cout<<"Array doubled: "<<dbscnt<<endl;
+	//cout<<size<<endl;
+	//cout<<totwrds<<endl;
+	cout<<"#"<<endl;
+	cout<<"Unique non-common words: "<<aindex<<endl;
+	cout<<"#"<<endl;
+	cout<<"Total non-common words: "<<unqwrds<<endl;
+	
+	file.close();
+	delete[] library;
+}
+
+
+/*int check(string ind)
+{
+	if (ind == "the")
+		return 1;
+	if (ind == "be")
+		return 1; 
+	if (ind == "to")
+		return 1; 
+	if (ind == "of")
+		return 1; 
+	if (ind == "and")
+		return 1; 
+	if (ind == "a")
+		return 1; 
+	if (ind == "in")
+		return 1; 
+	if (ind == "that")
+		return 1; 
+	if (ind == "have")
+		return 1; 
+	if (ind == "i")
+		return 1; 
+	if (ind == "it")
+		return 1; 
+	if (ind == "for")
+		return 1; 
+	if (ind == "not")
+		return 1; 
+	if (ind == "on")
+		return 1; 
+	if (ind == "with")
+		return 1; 
+	if (ind == "he")
+		return 1; 
+	if (ind == "as")
+		return 1; 
+	if (ind == "you")
+		return 1; 
+	if (ind == "do")
+		return 1; 
+	if (ind == "at")
+		return 1; 
+	if (ind == "this")
+		return 1; 
+	if (ind == "but")
+		return 1; 
+	if (ind == "his")
+		return 1; 
+	if (ind == "by")
+		return 1; 
+	if (ind == "from")
+		return 1; 
+	if (ind == "they")
+		return 1; 
+	if (ind == "we")
+		return 1; 
+	if (ind == "say")
+		return 1; 
+	if (ind == "her")
+		return 1; 
+	if (ind == "she")
+		return 1; 
+	if (ind == "or")
+		return 1; 
+	if (ind == "an")
+		return 1; 
+	if (ind == "will")
+		return 1; 
+	if (ind == "my")
+		return 1; 
+	if (ind == "one")
+		return 1; 
+	if (ind == "all")
+		return 1; 
+	if (ind == "would")
+		return 1; 
+	if (ind == "there")
+		return 1; 
+	if (ind == "their")
+		return 1; 
+	if (ind == "what")
+		return 1; 
+	if (ind == "so")
+		return 1; 
+	if (ind == "up")
+		return 1; 
+	if (ind == "out")
+		return 1; 
+	if (ind == "if")
+		return 1; 
+	if (ind == "about")
+		return 1; 
+	if (ind == "who")
+		return 1; 
+	if (ind == "get")
+		return 1; 
+	if (ind == "which")
+		return 1;
+	if (ind == "go")
+		return 1; 
+	if (ind == "me")
+		return 1;  
+	return 0;}*/
+void getStopwords(char *ignoreWordFileName, vector<string>& _vecIgnoreWords)
+{
+	int i = 0;
+	string igword;
+	ifstream ignore;
+	ignore.open(ignoreWordFileName);
+	while(ignore >> igword)
 	{
-		cout << "File unable to open!" << endl;
+		if(ignore.is_open())
+	      {
+            _vecIgnoreWords[i] = igword;
+            i++;
+	      }
+	}
+	ignore.close();       
+}
+bool isCommonWord(string word, vector<string>& _vecIgnoreWords)
+{
+	for(int i = 0; i<STOPWORD_LIST_SIZE; i++)
+	{
+		if(word == _vecIgnoreWords[i])
+		{
+			return true;
+		}
+	}
+	return false;
+}
+void printTopN(wordItem *&wordItemList, int topN)
+{
+	for (int i = 0; i < topN; ++i)
+	{
+		cout<<wordItemList[i].count<<"-"<<wordItemList[i].word<<endl;	
 	}
 }
